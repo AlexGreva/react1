16190c5811022191764544e83de42763b8d83966
AlexGreva <alex.grevtsev07@gmail.com>
2024-01-27 13:11:04 +0300

    update

--- src/components/restaurant/component.jsx
+++ src/components/restaurant/component.jsx
@@ -1,7 +1,7 @@
 import { Menu } from "./menu/component";
 export const Restaurant= ({info}) => {
   return <div>
-    <h1>{info.name}</h1>
+    <h1>{info.name}!</h1>
     <Menu list={info.menu}/>
   </div>;
 
