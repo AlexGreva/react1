2dd5e2dba566847d54e95780506ed4d011eb2dc0
AlexGreva <alex.grevtsev07@gmail.com>
2024-02-12 19:45:52 +0300

    3 lesson

--- src/components/restaurant/component.jsx
+++ src/components/restaurant/component.jsx
@@ -1,10 +1,12 @@
+import { ReviewForm } from "../review-form/component";
 import { Menu } from "./menu/component";
 import { Reviews } from "./reviews/component";
 export const Restaurant= ({info}) => {
   return <div>
-    <Reviews estimations={info.reviews}/>
     <h1>{info.name}!</h1>
     <Menu list={info.menu}/>
+    <Reviews estimations={info.reviews}/>
+    <ReviewForm/>
   </div>;
 
 };
